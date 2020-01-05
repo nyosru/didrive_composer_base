3a5f392d665d4297ace079669122e4144a46c3a0
Сергей <nyos@rambler.ru>
2020-01-05 09:18:46 +0500

    0.1.10

--- js.js
+++ js.js
@@ -1,5 +1,78 @@
 $().ready(function () {
 
+
+    /**
+     * jobdesc удаление оценки дня если есть параметры
+     * var $delete_ocenka_day = $(this).attr('delete_ocenka_day');
+     * var $delete_ocenka_sp = $(this).attr('delete_ocenka_sp');
+     * @param {type} $sp
+     * @param {type} $date
+     * @returns {Boolean}
+     */
+    function didrive__jobdesc__delete_day_ocenka($sp, $date, $s) {
+
+        // alert( $sp + ' = ' + $date + ' = ' + $s );
+
+        $.ajax({
+
+            type: 'POST',
+            url: '/vendor/didrive_mod/jobdesc/1/didrive/ajax.php',
+            dataType: 'json',
+            data: "action=delete_ocenka&sp=" + $sp + "&date=" + $date + "&s=" + $s,
+
+            // сoбытиe дo oтпрaвки
+            beforeSend: function ($data) {
+                // $div_res.html('<img src="/img/load.gif" alt="" border="" />');
+                // $this.css({"border": "2px solid orange"});
+            },
+            // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
+            success: function ($data) {
+
+//                // eсли oбрaбoтчик вeрнул oшибку
+                if ($data['status'] == 'error')
+                {
+                    console.log('не удаляем оценку дня - ошибка');
+//
+//                    // alert($data['error']); // пoкaжeм eё тeкст
+//                    //$div_res.html('<div class="warn warn">' + $data['html'] + '</div>');
+//                    $this.css({"border": "2px solid red"});
+//
+                }
+//                // eсли всe прoшлo oк
+                else
+                {
+                    console.log('не удаляем оценку дня - норм');
+//
+//                    // $div_res.html('<div class="warn good">' + $data['html'] + '</div>');
+//                    $this.css({"border": "2px solid green"});
+//                    $a_pole_price_id.html($a_text_in_pole_price_id);
+//
+                }
+
+            }
+            ,
+            // в случae нeудaчнoгo зaвeршeния зaпрoсa к сeрвeру
+            error: function (xhr, ajaxOptions, thrownError) {
+                // пoкaжeм oтвeт сeрвeрa
+                alert(xhr.status + ' ' + thrownError); // и тeкст oшибки
+            }
+
+// сoбытиe пoслe любoгo исхoдa
+// ,complete: function ($data) {
+// в любoм случae включим кнoпку oбрaтнo
+// $form.find('input[type="submit"]').prop('disabled', false);
+// }
+
+        }); // ajax-
+
+
+        return true;
+    }
+
+
+
+
+
     var didrive__edit_items_dop_pole = function (e) {
 
         // alert(e);
@@ -16,8 +89,43 @@ $().ready(function () {
         var $a_s = $(this).attr('edit_s');
         // var $a_s = $this.attr('edit_s');
 
-        var $a_pole_price_id = $('#'+$(this).attr('pole_price_id'));
+        var $a_pole_price_id = $('#' + $(this).attr('pole_price_id'));
         var $a_text_in_pole_price_id = $(this).attr('text_in_pole_price_id');
+
+
+
+
+        /**
+         * удаляем оценку если есть 2 переменные
+         * @type jQuery
+         */
+        var $delete_ocenka_date = $(this).attr('delete_ocenka_date');
+        var $delete_ocenka_sp = $(this).attr('delete_ocenka_sp');
+        var $delete_ocenka_s = $(this).attr('delete_ocenka_s');
+
+        if ($delete_ocenka_date == null || $delete_ocenka_sp == null || $delete_ocenka_s == null) {
+            console.log('не удаляем оценку дня');
+        } else {
+            console.log('удаляем оценку дня');
+            didrive__jobdesc__delete_day_ocenka($delete_ocenka_sp, $delete_ocenka_date, $delete_ocenka_s);
+        }
+
+
+
+
+
+
+
+
+//        if ($.fn.jobdesc_di__delete_day_ocenka2) {
+//            jobdesc_di__delete_day_ocenka2(1, 33);
+//            console.log('есть функция', 'jobdesc_di__delete_day_ocenka');
+//        } else {
+//            console.log('нет функции', 'jobdesc_di__delete_day_ocenka');
+//        }
+
+
+
 
         $.ajax({
 
@@ -49,8 +157,16 @@ $().ready(function () {
 
                     // $div_res.html('<div class="warn good">' + $data['html'] + '</div>');
                     $this.css({"border": "2px solid green"});
-                    $a_pole_price_id.html($a_text_in_pole_price_id);
                     
+                    /**
+                     * если есть эти параметры то печатаем в блок нужный текст
+                     */
+                    if ($a_pole_price_id == null || $a_text_in_pole_price_id == null) {
+                        
+                    } else {
+                        $a_pole_price_id.html($a_text_in_pole_price_id);
+                    }
+
                 }
 
             }
@@ -74,6 +190,5 @@ $().ready(function () {
     };
 
     $('body').on('keyup input', '.didrive__edit_items_dop_pole', $.debounce(1000, didrive__edit_items_dop_pole));
-
 
 });
