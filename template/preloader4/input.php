821254d0a2434512b3db387ae01ce27d2f2b5f6f
nyosru <nyos@rambler.ru>
2021-02-26 14:54:39 +0500

    2.15.0 обновил прелоадер 4

--- template/preloader4/input.php
+++ template/preloader4/input.php
@@ -1,6 +1,20 @@
 <?php
 
-$vv['dibody']['start'] .= '<link href="/template/preloader4/css.min.css" rel="stylesheet" />'
-    .file_get_contents($_SERVER['DOCUMENT_ROOT'].'/template/preloader4/up.htm');
+$vv['preloader_start'] = '<link href="/vendor/didrive/base/template/preloader4/css.min.css" rel="stylesheet" />'
+        . file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/vendor/didrive/base/template/preloader4/up.htm');
 
-$vv['dibody']['end'] .= file_get_contents($_SERVER['DOCUMENT_ROOT'].'/template/preloader4/down.htm');
+$vv['preloader_fin'] = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/vendor/didrive/base/template/preloader4/down.htm');
+
+/*
+
+вставить в шаблон 
+
+{% if preloader_start is defined %}
+{{ preloader_start }}
+{% endif %}
+
+{% if preloader_fin is defined %}
+{{ preloader_fin }}
+{% endif %}
+
+*/
